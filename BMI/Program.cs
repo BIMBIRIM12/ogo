34e40f61c6137a91622a45a474789d1bfb054697
BIMBIRIM12 <installer.233@gmail.com>
2022-02-16 14:35:28 +0300

    changes

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -1,20 +1,24 @@
-﻿M:
-Console.WriteLine("Hi,I can calculate your body mass index. Please enter your weight and height. Thanks ");
-Console.WriteLine("Please enter your weight(kilo)");
-double BMI;
-double weight = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Please enter your height(m)");
-double height = Convert.ToDouble(Console.ReadLine());
-if (weight > 0 && height > 0)
+﻿void BMI()
 {
-    height = height * height;
-    BMI = weight / height;
-    Console.WriteLine("Your BMI = " + BMI);
+M:
+    Console.WriteLine("Hi,I can calculate your body mass index. Please enter your weight and height. Thanks ");
+    Console.WriteLine("Please enter your weight(kilo)");
+    double BMI;
+    double weight = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Please enter your height(m)");
+    double height = Convert.ToDouble(Console.ReadLine());
+    if (weight > 0 && height > 0)
+    {
+        height = height * height;
+        BMI = weight / height;
+        Console.WriteLine("Your BMI = " + BMI);
 
+    }
+    else
+    {
+        Console.WriteLine("Please enter correct information");
+        goto M;
+    }
 }
-else
-{
-    Console.WriteLine("Please enter correct information");
-    goto M;
-}
+BMI();
 
