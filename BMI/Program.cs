9dcef6fb2c2a473ed85848cda17abf4a57fa15a9
BIMBIRIM12 <installer.233@gmail.com>
2022-02-23 19:13:41 +0300

    more comments

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -3,13 +3,17 @@
 namespace Bmi
 {
     class Program
-    {
+    {/// <summary>
+    /// Выводит в консоль индекс массы тела и его характеристики
+    /// </summary>
         
          static void Main()
-        {
+        {          
             BmiCalculator();
         }
-
+        /// <summary>
+        /// Запрашивает у пользователя рост и вес, также вызывает и выводит метод с рассчётом ИМТ и его характеристику в консоль
+        /// </summary>
         public static void BmiCalculator()
         {   
             var weight = ToDouble(GetValueFromUser("Введите вес:"));
@@ -20,12 +24,20 @@ namespace Bmi
             Console.WriteLine(bmi);
             Console.WriteLine(description);
         }
-
+        /// <summary>
+        /// Конвертирует принимаемое от пользователя значение в тип Double и возвращет его 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         private static double ToDouble (string value)
         {
             return double.Parse(value);
         }
-
+        /// <summary>
+        /// Запрашивает у пользователя значение роста и веса, и возвращает принятые значения
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         private static string GetValueFromUser (string message)
         {
             Console.WriteLine(message);
