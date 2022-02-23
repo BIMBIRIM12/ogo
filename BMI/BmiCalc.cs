8661ab9cf110222456b7bef1c17c9b9f8beb0554
BIMBIRIM12 <installer.233@gmail.com>
2022-02-23 12:53:02 +0300

    changes

---
+++ BMI/BmiCalc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bmi
+{
+    public class BmiCalc
+    {
+        public double Calculate(double weight, double height)
+        {
+            if (weight > 400 || weight < 10) 
+            {
+               Console.WriteLine("Вес должен находиться в диапозоне от 5 до 400 кг");              
+            }
+            if (height < 50 || height > 300) 
+            {
+                Console.WriteLine("Рост должен быть в диапозоне от 50 до 300 см");                          
+            }
+            double bmi;
+            bmi = (weight * 10000 / Math.Pow(height, 2));
+            return bmi;            
+        }
+       
+        public string GetDescription (double bmi)
+        {      
+           string description;
+            if (bmi < 16)
+            {
+                description = "Выраженный дефицит массы тела";
+            }
+            else if (bmi >= 16 && bmi <= 18.5)
+            {
+                description = "Недостаточная масса тела";
+
+            }
+            else if (bmi >= 18.5 && bmi <= 25)
+            {
+                description = "Всё в норме";
+            }
+            else if (bmi >= 25 && bmi <= 30)
+            {
+                description = "1-ая степень ожирения";
+            }
+            else if (bmi >= 30 && bmi <= 35)
+            {
+                description = "2-ая степень ожирения";
+            }
+            else
+            {
+                description = "3-ая степень ожирения";
+
+            }           
+             return description;         
+        }
+              
+    }
+}
--- BMI/Program.cs
+++ BMI/Program.cs
@@ -1,73 +1,37 @@
 ﻿using System;
 
-namespace BMI
+
+namespace Bmi
 {
     class Program
     {
         
-
-
          static void Main()
         {
-            Console.WriteLine("Привет, я могу посчитать индекс массы твоего тела и из этого сделать твой диагноз ");
-            Console.WriteLine("Пожалуйста введи свой вес (кг)");
-            double weight = double.Parse(Console.ReadLine());
-            Console.WriteLine("Пожалуйста введи свой рост (м)");
-            double height = double.Parse(Console.ReadLine());
-            double bmi =Result(weight, height);
-            string category = Category(bmi);
-            Console.WriteLine("Ваш рост: " + height + "\n Ваш вес: " + weight + "\n Ваш индекс масссы тела: " + bmi + "\n Ваш диагноз: " + category + "\n Хорошего дня! ");
-
-            
-           
-
-
-
+            BmiCalculator();
         }
-        static double Result (double weight, double height)
+
+        public static void BmiCalculator()
+        {   
+            var weight = ToDouble(GetValueFromUser("Введите вес:"));
+            var height = ToDouble(GetValueFromUser("Введите рост:"));
+            var bmiCalc = new BmiCalc();            
+            var bmi = bmiCalc.Calculate(weight, height);
+            var description = bmiCalc.GetDescription(bmi);           
+            Console.WriteLine(bmi);
+            Console.WriteLine(description);
+        }
+
+        private static double ToDouble (string value)
         {
-            double w = weight;
-            double h = height;
-            double bmi = w / Math.Pow(h, 2);
-            return bmi;
-           
-            
-            
+            return double.Parse(value);
         }
-        static string Category (double bmi)
+
+        private static string GetValueFromUser (string message)
         {
-            double b = bmi;
-            string category;
-            if (bmi <16)
-            {
-                category = "Выраженный дефицит массы тела";
-            }
-            else if ( bmi >= 16 && bmi <= 18.5)
-            {
-                category = "Недостаточная масса тела";
-
-            }
-            else if (bmi >= 18.5 && bmi <= 25)
-            {
-                category = "Всё в норме";
-            }
-            else if (bmi >= 25 && bmi <=30)
-            {
-                category = "1-ая степень ожирения";
-            }
-            else if (bmi >=30 && bmi <=35)
-            {
-                category = "2-ая степень ожирения";
-            }
-            else 
-            {
-                category = "3-ая степень ожирения";
-                
-            }
-            return category;
-           
-        }
-
+            Console.WriteLine(message);
+            return Console.ReadLine();       
+        }       
     }
 }
 
