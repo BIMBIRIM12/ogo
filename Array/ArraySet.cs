8da74676fb387685bb4ad2f9b4054734d6e841ab
BIMBIRIM12 <installer.233@gmail.com>
2022-02-24 15:27:32 +0300

    Changes

---
+++ Array/ArraySet.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    /// <summary>
+    /// Создаёт неотсортированный массив и заполняет его значениями
+    /// Сортирует его, возвращает его и выводит в консоль
+    /// Также вычисляет максимальное и минимальное значение массива и возвращает их
+    /// </summary>
+    public class ArraySet
+    {
+        /// <summary>
+        /// Создаёт и заполняет значениями неотсортированный массив
+        /// </summary>
+        /// <returns>Неотстортированный массив</returns>
+        public  int[] ArraySetUnSort()
+        {
+            int[] nums = new int[15];
+            Random a = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = a.Next(1, 1000);
+            }
+            return nums;
+        }
+        /// <summary>
+        /// Высчитывает и возвращает максимальное значение массива
+        /// </summary>
+        /// <param name="nums">Принимает неотсортированный массив значений</param>
+        /// <returns>Возвращает максимальное значение массива</returns>
+        public  int ArrayMax(int[] nums)
+        {
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                }
+            }
+            return max;
+        }
+        /// <summary>
+        /// Высчитывает минимальное значение массива 
+        /// </summary>
+        /// <param name="nums">Принимает неотсортированный массив значений</param>
+        /// <returns>Возвращает минимальное значение массива</returns>
+        public  int ArrayMin(int[] nums)
+        {
+            int min = 1000;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < min)
+                {
+                    min = nums[i];
+                }
+            }
+            return min;
+        }
+        /// <summary>
+        /// Сортирует неотсортированный массив значений по возрастанию, выводит его в консоль и возвращает
+        /// </summary>
+        /// <param name="nums">Принимает неотсортированный массив значений</param>
+        /// <returns>Возвращает сортированный массив значений</returns>
+        public int[] ArraySetSort(int[] nums)
+        {
+            int point;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    for (int l = i + 1; l > 0; l--)
+                    {
+                        if (nums[l] < nums[l - 1])
+                        {
+                            point = nums[l];
+                            nums[l] = nums[l - 1];
+                            nums[l - 1] = point;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Sorted");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i] + " ");
+            }
+            return nums;
+        }
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -1,52 +1,29 @@
-﻿void Array()
-{ int[] nums = new int[15];
-int max = 0;
-int min = 1000;
-Random a = new Random();
-for (int i = 0; i < nums.Length; i++)
-{
-    nums[i] = a.Next(1, 1000);
-    if (nums[i]>max)
-    {
-        max = nums[i];
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    }
-    if (nums[i]<min)
-    {
-        min = nums[i];
-
-    }
-}
-Console.WriteLine("Unsorted");
-for (int i = 0; i<nums.Length;i++)
+namespace Program
 {
-    Console.WriteLine(nums[i] + " "); 
-}
-int point;
-for (int i =0; i<nums.Length-1;i++)
-{
-    if (nums[i]>nums [i+1])
+    public class Array
     {
-        for (int l = i+1;l>0;l--)
+        static void Main (string[] args)
         {
-            if(nums[l]< nums[l-1])
-            {
-                point = nums[l];
-                nums[l] = nums[l - 1];
-                nums[l - 1] = point;
-            }
+            ArrayCalc();
         }
-    }
-}
-Console.WriteLine("\nSorted");
-for (int i=0;i<nums.Length;i++)
-    {
-    Console.Write(nums[i] + " ");
 
+        public static void ArrayCalc()
+        {
+            var arraySet = new ArraySet();
+            int[] nums  = arraySet.ArraySetUnSort();            
+            int[] numsort =arraySet.ArraySetSort(nums);
+            var max = arraySet.ArrayMax(nums);
+            var min =arraySet.ArrayMin(nums);
+            Console.WriteLine("Max " + max);
+            Console.WriteLine("Min " + min);
+        }  
     }
-Console.WriteLine("\nMax:" + max + "\nMin " + min);
-Console.ReadLine();
 }
-Array();
 
 
